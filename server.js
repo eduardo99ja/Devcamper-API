7d3c79a087c25d7993af8250fb00782e039ad5d6
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-02-06 18:46:14 -0600

    connecting to mongoDB

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-
-//Route files
-
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
 
 //Load env vars
 
 dotenv.config({ path: './config/config.env' })
 
+//Connect to database
+
+connectDB()
+
+//Route files
+
+const bootcamps = require('./routes/bootcamps')
+
 const app = express()
 
 //dev logging middleware
-if(process.env.NODE_ENV==='development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
@@ -22,7 +27,15 @@ if(process.env.NODE_ENV==='development'){
 app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
 )
+
+//Handle inhandled promises rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  //close server and exit process
+  server.close(() => process.exit(1))
+})
