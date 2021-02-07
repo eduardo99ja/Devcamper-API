95228e2e709e6fdea000ffcec0cd311d53f8f905
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-02-06 18:52:08 -0600

    colors in the console

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,9 +8,7 @@ exports.getBootcamps = (req, res, next) => {
 //? @route       GET /api/v1/bootcamps/:id
 //? @accesss     Public
 exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Get bootcamp ${req.params.id}` })
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` })
 }
 
 //? @desc        Create new bootcamp
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
 const connectDB = require('./config/db')
 
 //Load env vars
@@ -29,13 +30,15 @@ app.use('/api/v1/bootcamps', bootcamps)
 const PORT = process.env.PORT || 5000
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 )
 
 //Handle inhandled promises rejections
 
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`)
+  console.log(`Error: ${err.message}`.red)
   //close server and exit process
   server.close(() => process.exit(1))
 })
