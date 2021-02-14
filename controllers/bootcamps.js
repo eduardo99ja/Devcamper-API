5436f9ab156054085af3672c93091f1bd25994a5
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-02-13 23:33:58 -0600

    select and sorting fields

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,10 +7,36 @@ const geocoder = require('../utils/geocoder')
 //? @accesss     Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query
-  let queryStr = JSON.stringify(req.query)
- 
+  //copy req.query
+  const reqQuery = { ...req.query }
+
+  //Fields to exclude
+  const removeFields = ['select', 'sort']
+
+  //loop over removeFields and delete them from reqQuer
+  removeFields.forEach(param => delete reqQuery[param])
+  //create query String
+  let queryStr = JSON.stringify(reqQuery)
+
+  // create operators ($gt,$gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+  //finding resource
   query = Bootcamp.find(JSON.parse(queryStr))
+
+  //select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ')
+    query = query.select(fields)
+  }
+  // sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ')
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt ')
+  }
+
+  // executing query
   const bootcamps = await query
   res
     .status(200)
