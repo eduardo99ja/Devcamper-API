708d909370b5dd0ae87561162aa5f573cffd3d55
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-02-14 15:37:25 -0600

    Get all courses, and get courses for a specific bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,7 +8,13 @@ const {
   getBootcampsInRadius,
 } = require('../controllers/bootcamps')
 
+//include other resources routers
+const courseRouter = require('./courses')
+
 const router = express.Router()
+
+//Re-routes into other resource routers
+router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 router.route('/').get(getBootcamps).post(createBootcamp)
