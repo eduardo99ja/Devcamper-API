c58e4a790b8bc9e8e62dfd5ca2274a70f3928cc5
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-02-15 13:17:08 -0600

    photo upload for bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,6 +6,7 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps')
 
 //include other resources routers
@@ -17,6 +18,7 @@ const router = express.Router()
 router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
+router.route('/:id/photo').put(bootcampPhotoUpload)
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const fileupload = require('express-fileupload')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
@@ -27,6 +29,11 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+//file uploading
+app.use(fileupload())
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Mount routers
 
