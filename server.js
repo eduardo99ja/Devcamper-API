1ac954b4e9f3660f3470e0ee724f2b5498e9064e
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-02-18 19:52:20 -0600

    prevent xss atacks and securiting headers

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors')
 const fileupload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
@@ -42,6 +44,12 @@ app.use(fileupload())
 
 // Sanitize data
 app.use(mongoSanitize())
+
+// set security headers
+app.use(helmet())
+
+// Prevent XSS atacks
+app.use(xss())
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
